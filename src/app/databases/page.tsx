c381fe8113db9cc0dc007b2cabd4ab17cd7d1be6
Rasheem07 <mohdrasheem@icloud.com>
2025-01-13 21:07:18 +0530

    fixed error

--- src/app/databases/page.tsx
+++ src/app/databases/page.tsx
@@ -21,36 +21,47 @@ export default async function Page() {
       </h1>
       <div className="grid grid-cols-2 grid-flow-row gap-4 mt-6 w-full">
         {databases.length > 0 ? (
-          databases.map((database, i) => (
-            <Link
-              key={i}
-              href={`/databases/${database.name}`} // Adjust the link to point to the specific database dashboard
-              className="border bg-white border-teal-300 cursor-pointer group hover:bg-teal-500  rounded-md shadow-lg hover:shadow-xl p-6 min-w-full text-start transition-all duration-300 ease-in-out transform hover:scale-105"
-              aria-label="Go to database dashboard"
-            >
-              <div className="flex flex-col gap-4">
-                <div>
-                  <h2 className="text-2xl text-gray-700 font-semibold group-hover:text-zinc-50 transition-colors duration-300">
-                    {database.name}
-                  </h2>
-                  <p className="text-base text-zinc-500 group-hover:text-zinc-200 transition-colors duration-300">
-                    {database.description}
-                  </p>
+          databases.map(
+            (
+              database: {
+                id: string;
+                name: string;
+                description: string | null;
+                headers: string[];
+                types: string[];
+              },
+              i: number
+            ) => (
+              <Link
+                key={i}
+                href={`/databases/${database.name}`} // Adjust the link to point to the specific database dashboard
+                className="border bg-white border-teal-300 cursor-pointer group hover:bg-teal-500  rounded-md shadow-lg hover:shadow-xl p-6 min-w-full text-start transition-all duration-300 ease-in-out transform hover:scale-105"
+                aria-label="Go to database dashboard"
+              >
+                <div className="flex flex-col gap-4">
+                  <div>
+                    <h2 className="text-2xl text-gray-700 font-semibold group-hover:text-zinc-50 transition-colors duration-300">
+                      {database.name}
+                    </h2>
+                    <p className="text-base text-zinc-500 group-hover:text-zinc-200 transition-colors duration-300">
+                      {database.description}
+                    </p>
+                  </div>
+                  <div className="flex justify-between items-center mt-4">
+                    <button
+                      className="bg-teal-600 text-white py-2 px-4 rounded-md shadow-md hover:bg-teal-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-teal-400"
+                      aria-label="Go to dashboard"
+                    >
+                      Go to Dashboard
+                    </button>
+                    <span className="text-teal-600 group-hover:text-zinc-200 text-sm font-medium">
+                      Click to explore
+                    </span>
+                  </div>
                 </div>
-                <div className="flex justify-between items-center mt-4">
-                  <button
-                    className="bg-teal-600 text-white py-2 px-4 rounded-md shadow-md hover:bg-teal-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-teal-400"
-                    aria-label="Go to dashboard"
-                  >
-                    Go to Dashboard
-                  </button>
-                  <span className="text-teal-600 group-hover:text-zinc-200 text-sm font-medium">
-                    Click to explore
-                  </span>
-                </div>
-              </div>
-            </Link>
-          ))
+              </Link>
+            )
+          )
         ) : (
           <p>No databases found.</p>
         )}
