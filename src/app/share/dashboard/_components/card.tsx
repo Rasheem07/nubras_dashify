fc1c98df0f9e77f02e410fe5c7d55181142fed5c
Rasheem07 <mohdrasheem@icloud.com>
2025-01-22 21:52:11 +0530

    teal-700

--- src/app/share/dashboard/_components/card.tsx
+++ src/app/share/dashboard/_components/card.tsx
@@ -17,7 +17,7 @@ const SalesCard = ({
   return (
     <div className=" w-full bg-white rounded-lg shadow-lg overflow-hidden border border-gray-300">
       <div className="p-6">
-        <h2 className="text-2xl font-semibold text-black">{name}</h2>
+        <h2 className="text-2xl font-semibold text-teal-700">{name}</h2>
         <div className="mt-6">
           <p className="text-gray-600 text-sm">{title}</p>
           <p className="text-xl font-bold text-black mt-2">{Number(total).toFixed(2)}</p>
